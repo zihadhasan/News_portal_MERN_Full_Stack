33ce9a5fc00bb2979c6d5e0cfadf46ae577a42dc
zihadhasan <thedarkzihad@gmail.com>
2026-08-16 22:07:00 +0600

    axios fixed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://news-portal-mern-full-stack.onrender.com/api",
   withCredentials: true,
 });
 
